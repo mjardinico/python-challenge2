bc819bef29bc3d32b9548b035bcbb423253d70cb
Michael Jardinico <michael.jardinico@csun.edu>
2024-06-25 00:30:52 -0700

    modify PyBank/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,13 +16,41 @@ with open(csv_path) as csvfile:
 
     month_count = 0
     total_amount = 0
+    profit_loss = []
 
     for row in csvreader:
         # print(row)  
         month_count += 1
         total_amount += int(row[1])
+        profit_loss.append(int(row[1]))        
+    
+    #create a list comprehension for the values to reflect changes profit/loss changes for each month
+    changes = [profit_loss[i] - profit_loss[i-1] for i in range(1, len(profit_loss))]
+    changes_total = sum(changes)
+    average_changes = changes_total / len(changes)
+
+    #find the max() for greateast increase and min() for greatest decrease of values in the list - changes
+    greatest_increase = max(changes)
+    greatest_decrease = min(changes)
 
     print("Financial Analysis")    
     print("----------------------------")
     print(f"Total Months: {month_count}")
-    print(f"Total: ${total_amount}")
+    print(f"Total: ${total_amount}")  
+    print(f"Average Change: ${round(average_changes,2)}")
+    print(f"Greatest Increase in Profits: ${greatest_increase}")
+    print(f"Greatest Decrease in Profits: ${greatest_decrease}")
+
+    #check for greatest increase in profit based on the `changes` list
+    # greatest_increase = 0
+    # greatest_decrease = 0
+    # for change in changes:
+    #     if change > greatest_increase:
+    #         greatest_increase = change
+    #     elif change < greatest_decrease:
+    #         greatest_decrease = change
+
+    
+    
+
+        
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# python-challenge2
+# Title: Python Challenge
+# Author: Michael Jardinico
+# Version: 2
+# Date created: 6/22/24
+
+# Description:
+`This Python code analyzes the financial recors of the company, based on a given financial dataset called budget_data.csv.  The data is composed of two columns, 'Date' and 'Profit/Loss'`
